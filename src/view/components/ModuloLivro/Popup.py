f68e697094cf2c451d2bd07e6828ff974da4a2af
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-09-09 20:02:43 -0300

    adicionar grafico e melhorar o layout

--- src/view/components/ModuloLivro/Popup.py
+++ src/view/components/ModuloLivro/Popup.py
@@ -1,8 +1,20 @@
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QFrame, QLabel, QPushButton, QMessageBox
+from PyQt6.QtWidgets import (
+    QDialog,
+    QVBoxLayout,
+    QHBoxLayout,
+    QFrame,
+    QLabel,
+    QPushButton,
+    QMessageBox,
+    QSpacerItem,
+    QSizePolicy,
+)
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtCore import Qt
 from src.view.components.ModuloLivro.LeitorPDF import LeitorPDF
 from src.controller.telaPreviaLivro import dadosLivro
+from src.view.components.ModuloLivro.Grafico import Grafico
+
 
 class Popup(QDialog):
     def __init__(self, idLivro):
@@ -28,17 +40,23 @@ class Popup(QDialog):
 
         groupImagemBotao = QFrame(self)
         groupImagemBotao.setObjectName("groupImagemBotao")
+        groupImagemBotao.setFixedSize(300, 610)
         layout.addWidget(groupImagemBotao)
 
         ImagemBotaoLayout = QVBoxLayout()
         ImagemBotaoLayout.setObjectName("ImagemBotaoLayout")
+        ImagemBotaoLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         groupImagemBotao.setLayout(ImagemBotaoLayout)
 
         imagemCapaLivro = QImage.fromData(self.capaLivro)
         capa = QLabel(self)
         pixmap = QPixmap.fromImage(imagemCapaLivro)
         capa.setPixmap(pixmap)
-        ImagemBotaoLayout.addWidget(capa)
+
+        # colocar um espaço em cima e embaixo da capa, garantindo que ela fique no centro do frame
+        ImagemBotaoLayout.addStretch()
+        ImagemBotaoLayout.addWidget(capa, alignment=Qt.AlignmentFlag.AlignCenter)
+        ImagemBotaoLayout.addStretch()
 
         lerLivroBotao = QPushButton(groupImagemBotao)
         lerLivroBotao.setObjectName("lerLivroBotao")
@@ -59,22 +77,25 @@ class Popup(QDialog):
         h1 = QLabel("Informações do livro")
         infosLayout.addWidget(h1)
         h1.setObjectName("h1")
-        h1.setMaximumHeight(32)
+        h1.setMaximumHeight(50)
 
         tituloLabel = QLabel(f"titulo: {self.titulo}")
         tituloLabel.setObjectName("info")
-        tituloLabel.setMaximumHeight(16)
+        tituloLabel.setMaximumHeight(20)
         infosLayout.addWidget(tituloLabel)
 
         generoLabel = QLabel(f"genero: {self.genero}")
         generoLabel.setObjectName("info")
-        generoLabel.setMaximumHeight(16)
+        generoLabel.setMaximumHeight(20)
         infosLayout.addWidget(generoLabel)
 
         autorLabel = QLabel(f"autor: {self.autor}")
-        autorLabel.setMaximumHeight(16)
+        autorLabel.setMaximumHeight(20)
         autorLabel.setObjectName("info")
         infosLayout.addWidget(autorLabel)
+
+        grafico = Grafico(50, 50, self)
+        infosLayout.addWidget(grafico)
 
         button = QPushButton("Fechar", self)
         button.clicked.connect(self.accept)
@@ -84,9 +105,9 @@ class Popup(QDialog):
         self.leitor_pdf = None
 
     def abrirLeitorPDF(self):
-      if self.arquivoPDF:
-        # Criando e mostrando o leitor de PDF com o caminho do PDF
-        leitor_pdf = LeitorPDF(self.arquivoPDF)
-        leitor_pdf.exec()
-      else:
-        QMessageBox.critical(None, "Erro", "Esse livro não está disponível")
+        if self.arquivoPDF:
+            # Criando e mostrando o leitor de PDF com o caminho do PDF
+            leitor_pdf = LeitorPDF(self.arquivoPDF)
+            leitor_pdf.exec()
+        else:
+            QMessageBox.critical(None, "Erro", "Esse livro não está disponível")
