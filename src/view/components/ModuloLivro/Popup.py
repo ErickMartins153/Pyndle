a3f91ca25cdc6563cef4c936da64ec2977985b27
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-09-10 19:28:33 -0300

    adicionar o resto das informacoes

--- src/view/components/ModuloLivro/Popup.py
+++ src/view/components/ModuloLivro/Popup.py
@@ -27,7 +27,7 @@ class Popup(QDialog):
         self.anoPublicacao = self.dadosLivro[4]
         self.capaLivro = self.dadosLivro[5]
         self.arquivoPDF = self.dadosLivro[6]
-        self.pagTotal = self.dadosLivro[7]
+        self.qtdPaginasLabel = self.dadosLivro[7]
         self.setWindowTitle(self.titulo)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
         self.setFixedSize(700, 700)
@@ -79,20 +79,35 @@ class Popup(QDialog):
         h1.setObjectName("h1")
         h1.setMaximumHeight(50)
 
-        tituloLabel = QLabel(f"titulo: {self.titulo}")
+        tituloLabel = QLabel(f"Titulo: {self.titulo}")
         tituloLabel.setObjectName("info")
         tituloLabel.setMaximumHeight(20)
         infosLayout.addWidget(tituloLabel)
 
-        generoLabel = QLabel(f"genero: {self.genero}")
+        generoLabel = QLabel(f"Gênero: {self.genero}")
         generoLabel.setObjectName("info")
         generoLabel.setMaximumHeight(20)
         infosLayout.addWidget(generoLabel)
 
-        autorLabel = QLabel(f"autor: {self.autor}")
+        autorLabel = QLabel(f"Autor: {self.autor}")
         autorLabel.setMaximumHeight(20)
         autorLabel.setObjectName("info")
         infosLayout.addWidget(autorLabel)
+
+        anoLabel = QLabel(f"Ano: {self.anoPublicacao}")
+        anoLabel.setMaximumHeight(20)
+        anoLabel.setObjectName("info")
+        infosLayout.addWidget(anoLabel)
+
+        qtdPaginasLabel = QLabel(f"Total de páginas: {self.qtdPaginasLabel}")
+        qtdPaginasLabel.setMaximumHeight(20)
+        qtdPaginasLabel.setObjectName("info")
+        infosLayout.addWidget(qtdPaginasLabel)
+
+        avaliacaoLabel = QLabel(f"Avaliação: Placeholder")
+        avaliacaoLabel.setMaximumHeight(20)
+        avaliacaoLabel.setObjectName("info")
+        infosLayout.addWidget(avaliacaoLabel)
 
         grafico = Grafico(50, 50, self)
         infosLayout.addWidget(grafico)
@@ -101,8 +116,6 @@ class Popup(QDialog):
         button.clicked.connect(self.accept)
         infosLayout.addWidget(button)
         self.setLayout(layout)
-
-        self.leitor_pdf = None
 
     def abrirLeitorPDF(self):
         if self.arquivoPDF:
