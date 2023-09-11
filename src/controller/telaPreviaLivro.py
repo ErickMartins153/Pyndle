e7fb8ea7bd0721f9a1c8eed060799ad2a29871a2
Rony Rangel <141268611+RonyRangelSS@users.noreply.github.com>
2023-09-10 23:29:10 -0300

    Função de pegar avaliação e salvar pag total e lidas

--- src/controller/telaPreviaLivro.py
+++ src/controller/telaPreviaLivro.py
@@ -19,3 +19,34 @@ def dadosLivro(idLivro: int):
     else:
         print(f"O livro com ID {idLivro} não foi encontrado.")
         return None
+
+def pegarAvaliacao(id_livro, id_usuario):
+
+    sgbd.execute("SELECT avaliacao FROM usuariosLivros WHERE idLivro = ? AND idUsuario = ?", (id_livro, id_usuario,))
+    resultado = sgbd.fetchone()
+
+    if resultado:
+        return resultado[0]
+    else:
+        return "Livro não encontrado ou sem avaliação."
+
+            
+
+def salvarPag(id_usuario, id_livro, pag_lida):
+  
+    # Execute a instrução SQL para atualizar a coluna pagAtual
+    sgbd.execute("UPDATE usuariosLivros SET pagAtual = ? WHERE idUsuario = ? AND idLivro = ?", (pag_lida, id_usuario, id_livro))
+    
+    # Commit para salvar a alteração no banco de dados
+    connection.commit()
+    print("Pagina atualizada com sucesso.")
+
+
+def pagTotal(id_livro, pag_totais):
+    
+    # Execute a instrução SQL para inserir na coluna pagAtual
+    sgbd.execute("INSERT INTO livros (idLivro, pagTotal) VALUES (?, ?)", (id_livro, pag_totais))
+
+    # Commit para salvar a alteração no banco de dados
+    connection.commit()
+    print("Número de páginas inserido com sucesso")
