0125d7220612961d37ea015226b5a76ce78e2f9d
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-09-13 18:48:44 -0300

    adicionar formulário de cadastramento de livro

--- src/controller/telaPrincipal.py
+++ src/controller/telaPrincipal.py
@@ -1,17 +1,22 @@
 import sqlite3
 import fitz as PyMuPDF
+import io
+from PIL import Image
 
 conexao = sqlite3.connect("src/model/Pyndle.db")
 sgbd = conexao.cursor()
+
 
 def livrosCatalogo():
     """
     Função que dá todas as colunas dos nossos 7 livros do catalogo
     """
-    sgbd.execute("""
+    sgbd.execute(
+        """
     SELECT * FROM livros
      WHERE idLivro IN (1, 2, 3, 4, 5, 6, 7)
-     """)
+     """
+    )
 
     resultado = sgbd.fetchall()
 
@@ -19,41 +24,90 @@ def livrosCatalogo():
 
 
 def livrosPessoais(idUsuario: int):
-    sgbd.execute("""
+    sgbd.execute(
+        """
                  SELECT livros.* FROM livros 
                  JOIN usuariosLivros ON livros.idLivro = usuariosLivros.idLivro
-                 WHERE usuariosLivros.idUsuario = (?)""", (idUsuario,))
+                 WHERE usuariosLivros.idUsuario = (?)""",
+        (idUsuario,),
+    )
     resultado = sgbd.fetchall()
     return resultado
 
 
+def uploadLivro(arquivo):
+    try:
+        # Abre o arquivo PDF
+        livro = PyMuPDF.open(arquivo)
+        conteudoPdf = open(arquivo, "rb").read()
+        # Recupera a primeira página do PDF
+        primeira_pagina = livro[0]
 
-def uploadLivro(arquivo, idUsuario):
+        # Converte a primeira página em uma imagem
+        imagem = primeira_pagina.get_pixmap()
+
+        # Cria uma imagem PIL a partir do Pixmap
+        imagem_pil = Image.frombytes(
+            "RGB", [imagem.width, imagem.height], imagem.samples
+        )
+
+        # Salva a imagem em um buffer como formato JPEG
+        capaLivro = io.BytesIO()
+        imagem_pil.save(capaLivro, format="JPEG")
+
+        # Recupera os metadados do PDF
+        titulo = livro.metadata.get("title")
+        if not titulo.strip():
+            titulo = "Sem título"
+        autor = livro.metadata.get("author")
+        if not autor.strip():
+            autor = "Sem autor"
+        ano = livro.metadata.get("creationDate")[2:6]
+        if not ano.strip():
+            ano = "Sem data de criação"
+        genero = "Indeterminado"
+        paginas = livro.page_count
+
+        sgbd.execute(
+            """
+        INSERT INTO livros(arquivoPdf, titulo, genero, autor, anoPublicacao, pagTotal, capaLivro) 
+        VALUES (?,?,?,?,?,?, ?)
+        """,
+            (
+                conteudoPdf,
+                titulo,
+                genero,
+                autor,
+                ano,
+                paginas,
+                capaLivro.getvalue(),
+            ),
+        )
+        conexao.commit()
+        return sgbd.lastrowid
+
+    except Exception as e:
+        print(f"Ocorreu um erro ao processar o arquivo PDF: {e}")
+
+
+def apagarLivro(idLivro):
     """
-    Função para fazer o upload do arquivo PDF
-    :param arquivo: arquivo PDF que deseja salvar
-    :param idUsuario: id do usuário que está salvando o livro
+    Deleta o livro caso o usuário cancele o registro antes de finalizá-lo
     """
-    # Abre o arquivo PDF
-    livro = PyMuPDF.open(file)
+    sgbd.execute("SELECT idLivro FROM livros WHERE idLivro = ?", (idLivro,))
+    resultado = sgbd.fetchone()
+    conexao.commit()
 
-    # Recupera o conteúdo do PDF como bytes
-    pdf_content = open(file, 'rb').read()
+    if resultado[0] == idLivro:
+        sgbd.execute(
+            """DELETE FROM livros WHERE idLivro = (SELECT max(idLivro) FROM livros)"""
+        )
+        conexao.commit()
 
-    # Recupera os metadados do PDF
-    titulo = livro.metadata.get('title', 'Sem título')
-    autor = livro.metadata.get('author', 'Sem autor')
-    ano = livro.metadata.get('creationDate', 'Sem data de criação')
-    genero = input("Digite o gênero do livro: ")
-    review = input("Digite a review do livro: ")
-    paginas = livro.page_count
 
-    # Insere os dados do livro, incluindo o conteúdo do PDF, na tabela do banco de dados
-    sgbd.execute("""
-    INSERT INTO livros(arquivoPdf, titulo, genero, autor, anoPublicacao, pagTotal) 
-    VALUES (?,?,?,?,?,?)
-    """, (sqlite3.Binary(pdf_content), titulo, genero, autor, ano, review, paginas))
-
+def updateGenero(genero, idLivro):
+    sgbd.execute("UPDATE livros SET genero = ? WHERE idLivro = ?", (genero, idLivro))
+    conexao.commit()
 
 
 def buscarPalavraChave(arquivo, palavraChave: str):
@@ -65,7 +119,7 @@ def buscarPalavraChave(arquivo, palavraChave: str):
     """
     documento = PyMuPDF.open(arquivo)
     paginasEncontradas = []
-    
+
     for paginaNum, pagina in enumerate(documento, start=1):
         texto = pagina.get_text()
         if palavraChave.lower() in texto.lower():
@@ -83,26 +137,28 @@ def adicionarLivroCatalogo(titulo, genero, autor, anoPublicacao, arquivoPdf):
     # Abre o arquivo PDF
     livro = PyMuPDF.open(arquivoPdf)
     pagTotal = livro.page_count
-    arquivoPdfBytes = open(arquivoPdf, 'rb').read()
+    arquivoPdfBytes = open(arquivoPdf, "rb").read()
 
-    sgbd.execute("INSERT INTO livros (titulo, genero, autor, anoPublicacao, arquivoPdf) VALUES (?, ?, ?, ?, ?)",
-                 (titulo, genero, autor, anoPublicacao, arquivoPdfBytes))
+    sgbd.execute(
+        "INSERT INTO livros (titulo, genero, autor, anoPublicacao, arquivoPdf) VALUES (?, ?, ?, ?, ?)",
+        (titulo, genero, autor, anoPublicacao, arquivoPdfBytes),
+    )
 
 
 def filtrarGenero(genero=None, ordemAlfabetica=False):
-
     if ordemAlfabetica:
-        sgbd.execute("SELECT * FROM livros WHERE genero = ? ORDER BY titulo ASC", (genero,))
+        sgbd.execute(
+            "SELECT * FROM livros WHERE genero = ? ORDER BY titulo ASC", (genero,)
+        )
     else:
         sgbd.execute("SELECT * FROM livros WHERE genero = ?", (genero,))
 
     resultado = sgbd.fetchall()
 
-
     return resultado
 
-def filtrarAvaliacao(idUsuario, genero=None, avaliacao=None, ordemAlfabetica=False):
 
+def filtrarAvaliacao(idUsuario, genero=None, avaliacao=None, ordemAlfabetica=False):
     # Começa com uma consulta base que seleciona todos os campos de livros
     consulta = "SELECT * FROM livros WHERE 1"
 
@@ -135,6 +191,5 @@ def filtrarAvaliacao(idUsuario, genero=None, avaliacao=None, ordemAlfabetica=Fal
 
     # Recupera os resultados da consulta
     resultado = sgbd.fetchall()
-
 
     return resultado
